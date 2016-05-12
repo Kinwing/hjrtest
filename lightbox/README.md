3e9b118bbc5f721f7fa19734647989f5583efe7e
Kin-wing <hjr14@163.com>
2016-05-12 21:29:00 +0800

    dsa

---
+++ lightbox/README.md
@@ -0,0 +1,10 @@
+LightBoxWidget
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \com\widgets\KinwingWidgets\lightbox\LightBoxWidget::widget(['src' => Yii::$app->img->getImg($v['hash'])]); ?>
+
+```
---
+++ lightbox/assets/LightBoxAsset.php
@@ -0,0 +1,40 @@
+<?php
+namespace hjrtest\lightbox\assets;
+use yii\web\AssetBundle;
+use yii\web\View;
+
+
+/**
+ * Created by PhpStorm.
+ * User: win7
+ * Date: 2016/4/22
+ * Time: 16:17
+ */
+class LightBoxAsset extends AssetBundle
+{
+    public $js = [
+        'js/lightbox-2.6.min.js',
+    ];
+    public $css = [
+        'css/lightbox.css',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+    public $jsOptions = [
+        'position' => View::POS_HEAD
+    ];
+    public $cssOptions = [
+        'position' => View::POS_HEAD
+    ];
+
+
+    /**
+     * 初始化：sourcePath赋值
+     * @see \yii\web\AssetBundle::init()
+     */
+    public function init()
+    {
+        $this->sourcePath = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor';
+    }
+}
